63f3cfc59def7f1a83ff478e5d453acbd7c2cd78
RSM-git <rasmus.steen.mikkelsen@hotmail.com>
2025-04-23 16:08:20 +0200

    Merge

--- train_unimodal.py
+++ train_unimodal.py
@@ -24,42 +24,42 @@ def get_model_arch(
     s.__repr__ = repr.__get__(s)
     return repr(s)
 
+if __name__ == '__main__':
+    # debug
+    # os.environ["WANDB_MODE"] = "offline"
+    # enable_checkpointing = False
+    # max_epochs = 10
 
-# debug
-# os.environ["WANDB_MODE"] = "offline"
-# enable_checkpointing = False
-# max_epochs = 10
+    enable_checkpointing = True
+    max_epochs = 50
+    batch_size = 50
+    lr = 1e-3
+    dropout = 0.3
+    checkpoint_callback = L.pytorch.callbacks.ModelCheckpoint(
+        "models", monitor="val_acc", mode="max"
+    )
+    dm = EmbeddingDm(train_batch_size=batch_size)
+    # CLAP
+    # tags = ["CLAP", "unimodal", "audio"]
+    # model = ClapMlp(lr=lr, dropout=dropout)
 
-enable_checkpointing = True
-max_epochs = 50
-batch_size = 50
-lr = 1e-3
-dropout = 0.3
-checkpoint_callback = L.pytorch.callbacks.ModelCheckpoint(
-    "models", monitor="val_acc", mode="max"
-)
-dm = EmbeddingDm(train_batch_size=batch_size)
-# CLAP
-# tags = ["CLAP", "unimodal", "audio"]
-# model = ClapMlp(lr=lr, dropout=dropout)
+    # Whisper
+    # tags = ["Whisper", "unimodal", "audio"]
+    # model = WhisperMlp(lr=lr, dropout=dropout)
 
-# Whisper
-# tags = ["Whisper", "unimodal", "audio"]
-# model = WhisperMlp(lr=lr, dropout=dropout)
+    # wav2vec2
+    tags = ["wav2vec2", "unimodal", "audio"]
+    model = Wav2vec2Mlp(lr=lr, dropout=dropout)
 
-# wav2vec2
-tags = ["wav2vec2", "unimodal", "audio"]
-model = Wav2vec2Mlp(lr=lr, dropout=dropout)
-
-model_arch = get_model_arch(model, input_size=(1,), dtypes=[torch.long])
-wandb_logger = L.pytorch.loggers.WandbLogger(
-    project="LANoire",
-    entity="pydqn",
-    notes=model_arch,
-    config={"lr": lr, "batch_size": batch_size, "dropout": dropout},
-    tags=tags
-)
-trainer = L.Trainer(
-    max_epochs=max_epochs, logger=wandb_logger, callbacks=[checkpoint_callback]
-)
-trainer.fit(model, datamodule=dm)
+    model_arch = get_model_arch(model, input_size=(1,), dtypes=[torch.long])
+    wandb_logger = L.pytorch.loggers.WandbLogger(
+        project="LANoire",
+        entity="pydqn",
+        notes=model_arch,
+        config={"lr": lr, "batch_size": batch_size, "dropout": dropout},
+        tags=tags
+    )
+    trainer = L.Trainer(
+        max_epochs=max_epochs, logger=wandb_logger, callbacks=[checkpoint_callback]
+    )
+    trainer.fit(model, datamodule=dm)
