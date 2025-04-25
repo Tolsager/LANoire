49e651f3ae090af37f1440644178c4c7dcc709d4
Tolsager <victortolsagerolesen1@gmail.com>
2025-04-25 13:38:19 +0200

    add num workers

--- train_all.py
+++ train_all.py
@@ -9,22 +9,23 @@ if __name__ == "__main__":
     # max_epochs = 10
     enable_checkpointing = False
     max_epochs = 200
-    batch_size = 48
+    batch_size = 2
     lr = 1e-4
     dropout = 0.2
     weight_decay = 0.1
+    num_workers = 0
     checkpoint_callback = L.pytorch.callbacks.ModelCheckpoint(
         "models", monitor="val_acc", mode="max"
     )
-    dm = AllModalityDm(train_batch_size=batch_size)
+    dm = AllModalityDm(train_batch_size=batch_size, num_workers=num_workers)
 
-    tags = ["videomae", "trimodal", "gmu", "CLAP", "roberta"]
-    model = TextAudioVideo(dropout=dropout, weight_decay=weight_decay)
-
+    tags = ["videomae", "trimodal", "cat", "CLAP", "roberta"]
+    model = TextAudioVideo(feature_fusion="CONCAT")
     wandb_logger = L.pytorch.loggers.WandbLogger(
         project="LANoire",
         entity="pydqn",
-        config={"lr": lr, "batch_size": batch_size, "dropout": dropout, "weight_decay": weight_decay},
+        # config={"lr": lr, "batch_size": batch_size, "dropout": dropout, "weight_decay": weight_decay},
+        config={"lr": lr, "batch_size": batch_size},
         tags=tags
     )
     trainer = L.Trainer(
